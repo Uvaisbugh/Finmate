94e25d2922a1183a06cf6ca3007e2f44870d8254
Uvais Bughari <suvaisbughari@gmail.com>
2025-07-07 11:31:45 +0530

    Update home screen background and register plugin for macOS
    Changed the home screen background color to a lighter grey. Added registration for the path_provider_foundation plugin in the macOS Flutter plugin registrant.

--- lib/screens/home/screen_home.dart
+++ lib/screens/home/screen_home.dart
@@ -12,7 +12,7 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: const Text('Personal Money Manager'),
         centerTitle: true,
