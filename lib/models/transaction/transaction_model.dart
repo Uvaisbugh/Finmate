bc3c0dc9633bc77fe39756979b5d5d1550b5b73c
Uvais Bughari <suvaisbughari@gmail.com>
2025-07-08 11:13:32 +0530

    Create transaction_model.dart

---
+++ lib/models/transaction/transaction_model.dart
@@ -0,0 +1,24 @@
+import 'package:hive/hive.dart';
+part 'transaction_model.g.dart';
+
+@HiveType(typeId: 2)
+class Transaction {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String categoryId;
+
+  @HiveField(2)
+  final double amount;
+
+  @HiveField(3)
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.categoryId,
+    required this.amount,
+    required this.date,
+  });
+}
